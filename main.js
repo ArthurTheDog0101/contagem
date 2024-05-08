e272d0a9edae39cee1691898dd20a98f2bd18feb
ArthurTheDog0101 <129325489+ArthurTheDog0101@users.noreply.github.com>
2024-05-08 09:28:55 -0300

    bunda

--- main.js
+++ main.js
@@ -5,16 +5,18 @@ for(let i =0;i<botoes.length;i++){
     console.log(botoes[i])
     botoes[i].onclick = function(){
         for(let j=0;j<botoes.length; j++){
-       botoes[j].classList.remove("ativo")
-       textos[j].classList.remove("ativo")
-     } 
+        console.log("textos[j]")
+       botoes[j].classList.remove('ativo')
+       textos[j].classList.remove('ativo')
+            console.log(textos[j])
+    } 
         botoes[i].classList.add('ativo')
         textos[i].classList.add('ativo')
     }
 }
 
-const contadores = document.querySelectorAll('.contador') 
-const tempoObjetivo = new Date("2024-10-06T00:00:00"); 
+const contadores = document.querySelectorAll(".contador") 
+const tempoObjetivo1 = new Date("2024-10-06T00:00:00"); 
 let tempoatual = new Date()
 
-contadores[0].textContent = ((tempoObjetivo - tempoatual)/1000)/60/60/24
+contadores[0].textContent = ((tempoObjetivo1 - tempoatual)/1000)/60/60/24
